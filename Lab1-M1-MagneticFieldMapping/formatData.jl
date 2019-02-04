2b57dfd132281055bec40acfe3d235d722f68d8f
Ryan Wojtyla <ryan@droplit.io>
2019-02-04 16:58:55 -0500

    can produce plots of theoretical data

--- Lab1-M1-MagneticFieldMapping/formatData.jl
+++ Lab1-M1-MagneticFieldMapping/formatData.jl
@@ -3,6 +3,7 @@ using CSV
 
 data = CSV.File("2dMappingTesla.csv") |> DataFrame
 
+# return list of data points as triples
 function getDataPoints()
     dataPts = []
     
@@ -30,4 +31,16 @@ function getDataPoints()
     
 end
 
-println(getDataPoints())
+# convert column number (2:24) to radial distance value (-22:2:22)
+colNum2RadialDist(col) = col + (col - 26)
+
+# convert row number (1:16) to height value (3:2:33)
+rowNum2Height(row) = row + (row + 1)
+
+# data points for a row of data at the same height level
+getRow(z) = [ (z+2, colNum2RadialDist(col), data[col][z]) for col in 2:24 ]
+
+# a 2D array containing all the experimental data points as triples
+allPts = map(getRow(z), 1:16)
+
+println(allPts)
